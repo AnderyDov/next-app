075fb8e3d84b25bbe807a09e29b4fc55a43aa55e
Andrey <dovban.andreyy@rambler.ru>
2023-01-22 22:40:14 +0300

    commit

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link';
 import Menu from '../Menu/Menu';
 import styles from './Sidebar.module.css';
 import { SidebarProps } from './Sidebar.props';
+import Logo from '../logo.svg';
+import cn from 'classnames';
 
-export default function Sidebar({ ...props }: SidebarProps) {
+export default function Sidebar({ className, ...props }: SidebarProps) {
     return (
-        <div className={styles.sidebar} {...props}>
-            SIDEBAR
+        <div className={cn(className, styles.sidebar, {})} {...props}>
+            <Logo className={styles.logo} />
             <Link href='/search'>
                 <h2>Search</h2>
             </Link>
