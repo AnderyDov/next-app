bea09ff810b5648e8299f7d4f1b7e8a533ef8bc6
Andrey <dovban.andreyy@rambler.ru>
2023-01-26 10:01:08 +0300

    product_grid1

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,6 +1,9 @@
 import { ProductProps } from './Product.props';
 import styles from './Product.module.css';
 import cn from 'classnames';
+import { Cart } from '../Cart/Cart';
+import { Rate } from '../Rate/Rate';
+import { Tag } from '../Tag/Tag';
 
 export const Product = ({
     product,
@@ -8,8 +11,35 @@ export const Product = ({
     ...props
 }: ProductProps): JSX.Element => {
     return (
-        <div className={cn(className, styles.product)} {...props}>
-            {product.title}
-        </div>
+        <Cart className={styles.product}>
+            <div className={styles.logo}>
+                <img src={process.env.NEXT_PUBLIC_DOMAIN} alt={product.title} />
+            </div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.price}>{product.price}</div>
+            <div className={styles.credit}>{product.credit}</div>
+            <div className={styles.rate}>
+                <Rate rate={product.reviewAvg ?? product.initialRating} />
+            </div>
+            <div className={styles.tags}>
+                {product.tags.map((el) => (
+                    <Tag key={el} color='ghost'>
+                        {el}
+                    </Tag>
+                ))}
+            </div>
+            <div className={styles.priceTitle}>цена</div>
+            <div className={styles.creditTitle}>кредит</div>
+            <div className={styles.reviewCount}>
+                {product.reviewCount} отзывов
+            </div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.title}>{product.title}</div>
+        </Cart>
     );
 };
