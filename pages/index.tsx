e6f4e80663ffe7f6f6e0b15d01881a554ca85491
Andrey <dovban.andreyy@rambler.ru>
2023-02-12 20:12:26 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,15 @@ import { API } from '../helpers/api';
 import { IMenu } from '../interfaces/menu.interface';
 import withLauout from '../layout/Layout';
 import styles from '../styles/Home.module.css';
-import i1 from '../image/1.jpg';
-import i2 from '../image/2.jpg';
-import i3 from '../image/3.jpg';
-import i4 from '../image/4.jpg';
-import i5 from '../image/5.jpg';
-import i6 from '../image/6.jpg';
-import i7 from '../image/7.jpg';
-import i8 from '../image/8.jpg';
 
 function Home() {
     return (
         <div className={styles.home}>
             <div className={styles.image}>
                 <Image
-                    src={i1}
+                    src={
+                        'https://images.unsplash.com/photo-1573495628366-64d875afdf1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGl0fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
@@ -28,7 +22,9 @@ function Home() {
             </div>
             <div className={styles.image}>
                 <Image
-                    src={i2}
+                    src={
+                        'https://images.unsplash.com/photo-1573495804660-b6b271f4c3f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fGl0fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
@@ -38,7 +34,9 @@ function Home() {
             </div>
             <div className={styles.image}>
                 <Image
-                    src={i3}
+                    src={
+                        'https://images.unsplash.com/photo-1573496528681-9b0f4fb0c660?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM1fHxpdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
@@ -48,7 +46,9 @@ function Home() {
             </div>
             <div className={styles.image}>
                 <Image
-                    src={i4}
+                    src={
+                        'https://images.unsplash.com/photo-1573166855576-5e6dca380e7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQyfHxpdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
@@ -58,7 +58,9 @@ function Home() {
             </div>
             <div className={styles.image}>
                 <Image
-                    src={i5}
+                    src={
+                        'https://images.unsplash.com/photo-1573495627361-d9b87960b12d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTg3fHxpdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
@@ -68,7 +70,9 @@ function Home() {
             </div>
             <div className={styles.image}>
                 <Image
-                    src={i6}
+                    src={
+                        'https://images.unsplash.com/photo-1623018035813-9cfb5b502e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjE1fHxpdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
@@ -78,7 +82,9 @@ function Home() {
             </div>
             <div className={styles.image}>
                 <Image
-                    src={i7}
+                    src={
+                        'https://images.unsplash.com/photo-1573219093925-cea25da23f54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjIxfHxpdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
@@ -88,7 +94,9 @@ function Home() {
             </div>
             <div className={styles.image}>
                 <Image
-                    src={i8}
+                    src={
+                        'https://images.unsplash.com/photo-1559085715-23704349e326?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjM2fHxpdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=1600&q=60'
+                    }
                     alt={'image'}
                     fill
                     priority
